97d5e7c996df47ed872f8f09086f54d0cd88a57f
Arpit Verma <arpitv970@gmail.com>
2023-12-20 19:30:23 +0530

    basic nextjs setup done

---
+++ components/common/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <h1>UI Schema Form</h1>
+      <ModeToggle />
+    </nav>
+  )
+}
+
+export default Navbar
