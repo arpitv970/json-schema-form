76fce189c086736dc54ce0d3335b78fa8efb6c2f
Arpit Verma <arpitv970@gmail.com>
2023-12-24 17:11:51 +0530

    added requried start

--- components/jsonForm-ui/index.tsx
+++ components/jsonForm-ui/index.tsx
@@ -4,11 +4,14 @@ import { Input } from '../ui/input'
 import { Label } from '../ui/label'
 
 const RenderInput = (field: FormSchemaType) => {
-  const { placeholder, label, uiType } = field
+  const { placeholder, label, uiType, sort, level, icon, jsonKey, validate, pattern, description, subParameters } = field
   return (
     <>
-      <Label>{label}</Label>
+      <Label>{label}{validate.required ?
+        <span className='text-red-500'>*</span> : ''}
+      </Label>
       <Input
+        hidden={validate.immutable}
         type={uiType}
         placeholder={placeholder}
       />
